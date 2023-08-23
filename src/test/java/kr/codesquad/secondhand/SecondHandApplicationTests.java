4e2d4baf7a1b25dde1c0d18d48555bb10b7b153e
JeeIn Lee <jinny.l.dev@gmail.com>
2023-08-23 15:41:02 +0900

    [chore] Spring Boot 개발 환경 세팅 (#2)

---
+++ src/test/java/kr/codesquad/secondhand/SecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad.secondhand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
