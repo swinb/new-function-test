e5dd58e4a34d96e5d07898e02640056fe37c5991
jinny-l <jinny.l.dev@gmail.com>
2023-08-28 15:14:01 +0900

    feat: Oauth 서버별 유저 정보와 매핑되는 클래스 구현

---
+++ be/src/main/java/kr/codesquad/secondhand/oauth/domain/OAuthAttributes.java
@@ -0,0 +1,40 @@
+package kr.codesquad.secondhand.oauth.domain;
+
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Oauth 서버별 OauthProfile 정보를 갖고 있는 Enum 클래스
+ */
+@RequiredArgsConstructor
+public enum OAuthAttributes {
+
+    GITHUB("github") {
+        @Override
+        public OAuthProfile of(Map<String, Object> attributes) {
+            return OAuthProfile.builder()
+                    .email((String) attributes.get("email"))
+                    .name((String) attributes.get("name"))
+                    .imageUrl((String) attributes.get("avatar_url"))
+                    .build();
+        }
+    },
+    KAKAO("kakao") {
+        @Override
+        public OAuthProfile of(Map<String, Object> attributes) {
+            Map<String, Object> info = (Map<String, Object>) attributes.get("kakao_account");
+            Map<String, Object> profile = (Map<String, Object>) info.get("profile");
+            return OAuthProfile.builder()
+                    .email((String) info.get("email"))
+                    .name((String) profile.get("nickname"))
+                    .imageUrl((String) profile.get("profile_image_url"))
+                    .build();
+        }
+    };
+
+    @Getter
+    public final String providerName;
+
+    public abstract OAuthProfile of(Map<String, Object> attributes);
+}
---
+++ be/src/main/java/kr/codesquad/secondhand/oauth/domain/OAuthProfile.java
@@ -0,0 +1,41 @@
+package kr.codesquad.secondhand.oauth.domain;
+
+import java.util.Arrays;
+import java.util.Map;
+import kr.codesquad.secondhand.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * Oauth 리소스 서버의 유저 정보와 바인딩되는 객체
+ */
+@Getter
+public class OAuthProfile {
+
+    private final String email;
+    private final String name;
+    private final String imageUrl;
+
+    @Builder
+    public OAuthProfile(String email, String name, String imageUrl) {
+        this.email = email;
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    public static OAuthProfile of(String providerName, Map<String, Object> attributes) {
+        return Arrays.stream(OAuthAttributes.values())
+                .filter(provider -> providerName.equals(provider.providerName))
+                .findFirst()
+                .orElseThrow() // TODO: 예외처리
+                .of(attributes);
+    }
+
+    public Member toMember() {
+        return Member.builder()
+                .email(email)
+                .nickname(name)
+                .profileImgUrl(imageUrl)
+                .build();
+    }
+}
